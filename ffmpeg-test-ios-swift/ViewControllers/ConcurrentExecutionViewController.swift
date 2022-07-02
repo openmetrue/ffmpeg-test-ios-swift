d84bc7fb8674f218987c401074accfe0314128dc
openmetrue <openmetrue@gmail.com>
2022-07-02 17:38:45 +0800

    compete ConcurrentExecutionVC UI

--- ffmpeg-test-ios-swift/ViewControllers/ConcurrentExecutionViewController.swift
+++ ffmpeg-test-ios-swift/ViewControllers/ConcurrentExecutionViewController.swift
@@ -56,31 +56,31 @@ class ConcurrentExecutionViewController: UIViewController {
         }
     }
     
-    @IBAction func encode1Clicked(sender:AnyObject!) {
+    @objc func encode1Clicked() {
         self.encodeVideo(1)
     }
     
-    @IBAction func encode2Clicked(sender:AnyObject!) {
+    @objc func encode2Clicked() {
         self.encodeVideo(2)
     }
     
-    @IBAction func encode3Clicked(sender:AnyObject!) {
+    @objc func encode3Clicked() {
         self.encodeVideo(3)
     }
     
-    @IBAction func cancel1Button(sender:AnyObject!) {
+    @objc func cancel1ButtonClicked() {
         self.cancel(1)
     }
     
-    @IBAction func cancel2Button(sender:AnyObject!) {
+    @objc func cancel2ButtonClicked() {
         self.cancel(2)
     }
     
-    @IBAction func cancel3Button(sender:AnyObject!) {
+    @objc func cancel3ButtonClicked() {
         self.cancel(3)
     }
     
-    @IBAction func cancelAllButton(sender:AnyObject!) {
+    @objc func cancelAllButtonClicked() {
         self.cancel(0)
     }
     
@@ -153,14 +153,84 @@ class ConcurrentExecutionViewController: UIViewController {
 extension ConcurrentExecutionViewController {
     private func setupViews() {
         view.addSubview(header)
+        view.addSubview(encode1Button)
+        encode1Button.setTitle("ENCODE 1", for: .normal)
+        encode1Button.addTarget(self, action: #selector(encode1Clicked), for: .touchDown)
+        view.addSubview(encode2Button)
+        encode2Button.setTitle("ENCODE 2", for: .normal)
+        encode2Button.addTarget(self, action: #selector(encode2Clicked), for: .touchDown)
+        view.addSubview(encode3Button)
+        encode3Button.setTitle("ENCODE 3", for: .normal)
+        encode3Button.addTarget(self, action: #selector(encode3Clicked), for: .touchDown)
+        view.addSubview(cancel1Button)
+        cancel1Button.setTitle("CANCEL 1", for: .normal)
+        cancel1Button.addTarget(self, action: #selector(cancel1ButtonClicked), for: .touchDown)
+        view.addSubview(cancel2Button)
+        cancel2Button.setTitle("CANCEL 2", for: .normal)
+        cancel2Button.addTarget(self, action: #selector(cancel2ButtonClicked), for: .touchDown)
+        view.addSubview(cancel3Button)
+        cancel3Button.setTitle("CANCEL 3", for: .normal)
+        cancel3Button.addTarget(self, action: #selector(cancel3ButtonClicked), for: .touchDown)
+        view.addSubview(cancelAllButton)
+        cancelAllButton.setTitle("CANCEL ALL", for: .normal)
+        cancelAllButton.addTarget(self, action: #selector(cancelAllButtonClicked), for: .touchDown)
+        view.addSubview(outputText)
     }
     private func setupLayout() {
         header.translatesAutoresizingMaskIntoConstraints = false
+        encode1Button.translatesAutoresizingMaskIntoConstraints = false
+        encode2Button.translatesAutoresizingMaskIntoConstraints = false
+        encode3Button.translatesAutoresizingMaskIntoConstraints = false
+        cancel1Button.translatesAutoresizingMaskIntoConstraints = false
+        cancel2Button.translatesAutoresizingMaskIntoConstraints = false
+        cancel3Button.translatesAutoresizingMaskIntoConstraints = false
+        cancelAllButton.translatesAutoresizingMaskIntoConstraints = false
+        outputText.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             header.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
             header.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             header.heightAnchor.constraint(equalToConstant: 50),
-            header.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1)
+            header.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1),
+            
+            encode1Button.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 40),
+            encode1Button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -110),
+            encode1Button.heightAnchor.constraint(equalToConstant: 32),
+            encode1Button.widthAnchor.constraint(equalToConstant: 100),
+            
+            encode2Button.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 40),
+            encode2Button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            encode2Button.heightAnchor.constraint(equalToConstant: 32),
+            encode2Button.widthAnchor.constraint(equalToConstant: 100),
+            
+            encode3Button.topAnchor.constraint(equalTo: header.bottomAnchor, constant: 40),
+            encode3Button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 110),
+            encode3Button.heightAnchor.constraint(equalToConstant: 32),
+            encode3Button.widthAnchor.constraint(equalToConstant: 100),
+            
+            cancel1Button.topAnchor.constraint(equalTo: encode1Button.bottomAnchor, constant: 40),
+            cancel1Button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -150),
+            cancel1Button.heightAnchor.constraint(equalToConstant: 32),
+            cancel1Button.widthAnchor.constraint(equalToConstant: 90),
+            
+            cancel2Button.topAnchor.constraint(equalTo: encode2Button.bottomAnchor, constant: 40),
+            cancel2Button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -55),
+            cancel2Button.heightAnchor.constraint(equalToConstant: 32),
+            cancel2Button.widthAnchor.constraint(equalToConstant: 90),
+            
+            cancel3Button.topAnchor.constraint(equalTo: encode2Button.bottomAnchor, constant: 40),
+            cancel3Button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 40),
+            cancel3Button.heightAnchor.constraint(equalToConstant: 32),
+            cancel3Button.widthAnchor.constraint(equalToConstant: 90),
+            
+            cancelAllButton.topAnchor.constraint(equalTo: encode3Button.bottomAnchor, constant: 40),
+            cancelAllButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 145),
+            cancelAllButton.heightAnchor.constraint(equalToConstant: 32),
+            cancelAllButton.widthAnchor.constraint(equalToConstant: 110),
+            
+            outputText.topAnchor.constraint(equalTo: cancelAllButton.bottomAnchor, constant: 40),
+            outputText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            outputText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            outputText.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40)
         ])
     }
 }
